f201d62c23108bbb13d6d7e7bdf59062cfae9f34
Alexey <112696799+Dar-Tei@users.noreply.github.com>
2023-05-25 14:02:49 +0300

    Delete VPSv2 directory

--- VPSv2/JS/preloader.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var preloader = document.getElementById('preloader');
-  preloader.classList.add('loaded'); 
-  window.setTimeout(function () {
-    preloader.classList.remove('loaded');
-    preloader.classList.add('hided'); 
-  }, 500); 
-}
